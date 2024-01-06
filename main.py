32595e54b75f6d0eb5ababedbb19cc4385949428
DenK0 <denidinamo@gmail.com>
2024-01-06 03:47:50 +0200

    _-_-

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from collections import UserDict
-from datetime import datetime
-import csv
+from datetime import datetime, timedelta
+import pickle
 
 
 class Field:
     def __init__(self, value):
-        self.__value = value
+        self._value = value
 
     def __str__(self):
-        return str(self.__value)
+        return str(self._value)
 
     @property
     def value(self):
-        return self.__value
+        return self._value
 
     @value.setter
     def value(self, new_value):
-        self.__value = new_value
+        self._value = new_value
 
 
 class Name(Field):
@@ -25,29 +25,45 @@ class Name(Field):
 
 class Phone(Field):
     def __init__(self, value):
-        if not value.isdigit() or len(value) != 10:
+        self._value = None
+        self.value = value
+
+    @Field.value.setter
+    def value(self, new_value):
+        if not new_value.isdigit() or len(new_value) != 10:
             raise ValueError(
                 "Phone number must be 10 digits and contain only numbers")
-        super().__init__(value)
+        self._value = new_value
 
 
 class Birthday(Field):
     def __init__(self, value=None):
-        if value is not None:
-            value = self.__validate_birthday(value)
-        super().__init__(value)
-
-    def __validate_birthday(self, value):
-        try:
-            return datetime.strptime(value, "%Y-%m-%d").date()
-        except ValueError:
-            raise ValueError(
-                "Invalid birthday format. Please use the format 'YYYY-MM-DD'")
+        self._value = None
+        self.value = value
 
     @Field.value.setter
     def value(self, new_value):
         if new_value is not None:
-            self.__value = self.__validate_birthday(new_value)
+            try:
+                datetime.strptime(new_value, "%Y-%m-%d")
+            except ValueError:
+                raise ValueError(
+                    "Invalid birthday format. Please use YYYY-MM-DD")
+        self._value = new_value
+
+    def days_to_birthday(self):
+        if self._value is None:
+            return None
+        today = datetime.now().date()
+        birthday = datetime.strptime(
+            self._value, "%Y-%m-%d").date().replace(year=today.year)
+        if birthday < today:
+            birthday = birthday.replace(year=today.year + 1)
+        return (birthday - today).days
+
+    def to_string(self):
+        date = datetime.strptime(self._value, "%Y-%m-%d")
+        return date.strftime('%Y-%m-%d')
 
 
 class Record:
@@ -81,26 +97,11 @@ class Record:
                 return p
         return None
 
-    def days_to_birthday(self):
-        if self.birthday.value is None:
-            return None
-        today = datetime.today().date()
-        next_birthday = datetime(
-            today.year, self.birthday.value.month, self.birthday.value.day).date()
-        if next_birthday < today:
-            next_birthday = datetime(
-                today.year + 1, self.birthday.value.month, self.birthday.value.day).date()
-        return (next_birthday - today).days
-
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 
 class AddressBook(UserDict):
-    def __init__(self, filename):
-        super().__init__()
-        self.filename = filename
-
     def add_record(self, record):
         self.data[record.name.value] = record
 
@@ -111,66 +112,163 @@ class AddressBook(UserDict):
         if name in self.data:
             del self.data[name]
 
-    def save(self):
-        with open(self.filename, 'w', newline='') as file:
-            writer = csv.writer(file)
-            for record in self.data.values():
-                writer.writerow([record.name.value, record.birthday.value] +
-                                [phone.value for phone in record.phones])
-
-    def load(self):
-        self.data.clear()
-        with open(self.filename, 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                name = row[0]
-                birthday = row[1] if row[1] != '' else None
-                phones = row[2:]
-                record = Record(name, birthday)
-                for phone in phones:
-                    record.add_phone(phone)
-                self.add_record(record)
-
     def search(self, query):
         results = []
         for record in self.data.values():
-            if query in record.name.value or any(query in phone.value for phone in record.phones):
+            if query.lower() in record.name.value.lower():
                 results.append(record)
+            for phone in record.phones:
+                if query in phone.value:
+                    results.append(record)
+                    break
         return results
 
+    def save(self, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(self.data, file)
+
+    def load(self, filename):
+        with open(filename, 'rb') as file:
+            self.data = pickle.load(file)
+
     def __iter__(self):
-        return self.iterator()
-
-    def iterator(self, N=1):
-        records = list(self.data.values())
-        for i in range(0, len(records), N):
-            yield [str(record) for record in records[i:i+N]]
+        return iter(self.data.values())
 
 
-# Example:
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError as e:
+            print(e)
 
-# Add a record
-address_book = AddressBook("address_book.csv")
+    return inner
 
-# Record 1
-record1 = Record("Denis Yevtushenko", "1988-09-26")
-record1.add_phone("0637344967")
-address_book.add_record(record1)
 
-# Record 2
-record2 = Record("Nataliia Yevtushenko", "1987-01-30")
-record2.add_phone("0638280932")
-address_book.add_record(record2)
+class Menu:
+    def __init__(self):
+        self.address_book = AddressBook()
+        self.choices = {
+            "1": self.show_contacts,
+            "2": self.add_contact,
+            "3": self.edit_contact,
+            "4": self.delete_contact,
+            "5": self.find_contact,
+            "6": self.days_until_birthday,
+            "7": self.search_contacts,
+            "8": self.save_address_book,
+            "9": self.load_address_book,
+            "0": self.quit
+        }
 
-# Record 3
-record3 = Record("Vasylyna Yevtushenko", "2014-01-04")
-record3.add_phone("0930635314")
-address_book.add_record(record3)
+    def menu(self):
+        print("""
+        1. Show contacts
+        2. Add contact
+        3. Edit contact
+        4. Delete contact
+        5. Find contact
+        6. Days until birthday
+        7. Search contacts
+        8. Save address book
+        9. Load address book             
+        0. Quit
+        """)
 
-# Save the address book
-address_book.save()
+    def run(self):
+        while True:
+            self.menu()
+            choice = input("Enter an option: ")
+            action = self.choices.get(choice)
+            if action:
+                action()
+            else:
+                print(f"{choice} is not a valid choice")
 
-# Search for contacts
-results = address_book.search(input("Enter search query: "))
-for result in results:
-    print(result)
+    def show_contacts(self):
+        if self.address_book:
+            for contact in self.address_book:
+                print(contact)
+        else:
+            print("Address book is empty")
+
+    def days_until_birthday(self):
+        name = input("Enter the name of the contact: ")
+        contact = self.address_book.find(name)
+        if contact:
+            birthday = datetime.strptime(
+                contact.birthday.to_string(), '%Y-%m-%d')
+            today = datetime.today()
+            next_birthday = datetime(today.year, birthday.month, birthday.day)
+            if today > next_birthday:
+                next_birthday = datetime(
+                    today.year+1, birthday.month, birthday.day)
+            delta = next_birthday - today
+            print(f"There are {delta.days} days until {name}'s birthday.")
+        else:
+            print("Contact not found.")
+
+    @input_error
+    def add_contact(self):
+        name = input("Enter name: ")
+        phones = input("Enter phone numbers separated by space: ").split()
+        birthday = input("Enter birthday (optional, format: YYYY-MM-DD): ")
+        record = Record(name, birthday)
+        for phone in phones:
+            record.add_phone(phone)
+        self.address_book.add_record(record)
+
+    @input_error
+    def edit_contact(self):
+        name = input("Enter name: ")
+        record = self.address_book.find(name)
+        if record:
+            phone = input("Enter phone number: ")
+            new_phone = input("Enter new phone number: ")
+            record.edit_phone(phone, new_phone)
+        else:
+            print("Contact does not exist")
+
+    @input_error
+    def delete_contact(self):
+        name = input("Enter name: ")
+        self.address_book.delete(name)
+
+    @input_error
+    def find_contact(self):
+        name = input("Enter name: ")
+        record = self.address_book.find(name)
+        if record:
+            print(record)
+        else:
+            print("Contact does not exist")
+
+    def search_contacts(self):
+        query = input("Enter search query: ")
+        results = self.address_book.search(query)
+        if results:
+            for contact in results:
+                print(contact)
+        else:
+            print("No matching contacts found")
+
+    def save_address_book(self):
+        filename = input("Enter filename to save address book: ")
+        self.address_book.save(filename)
+        print("Address book saved successfully")
+
+    def load_address_book(self):
+        filename = input("Enter filename to load address book from: ")
+        try:
+            self.address_book.load(filename)
+            print("Address book loaded successfully")
+        except FileNotFoundError:
+            print("File not found")
+
+    def quit(self):
+        print("Bye")
+        exit()
+
+
+if __name__ == "__main__":
+    Menu().run()
